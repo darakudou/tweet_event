69a280b2c04000e6c20e3878162eaeb8499a0866
中島祐樹 <blackrx.robo.vaio@gmail.com>
2016-06-16 19:32:44 +0900

    づや会を取得出来るように修正

--- tweet.py
+++ tweet.py
@@ -1,17 +1,23 @@
-#!/usr/bin/env python
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
 import sys  
 sys.path.append("site-packages")
  
 import datetime, twython, platform
-import urllib.request as urllib2
+import urllib
+import urllib.request
+import urllib.parse
 from bs4 import BeautifulSoup
+import requests
 import re
 import time
 import configparser
+import json
 
 def main():
 
+
+    #doorkeeperからギーラボとnsegさんのイベント情報を取得する
     comm_names = ['nseg','glnagano']
     #コミュニティの数でループ
     for comm_name in comm_names:
@@ -28,15 +34,42 @@ def main():
             print('---- print works ---')
             print(e.message)
             print('----------print end-')
+    #connpassからづや会の情報を取得する
+    try:
+        #日本語部分をエンコードしてバイト型にする
+        param = {"keyword":"づや会".encode(encoding="utf-8")}
+        #urlエンコードでurlっぽくしてくれる？？
+        encodedParams = urllib.parse.urlencode(param)
+        with urllib.request.urlopen("http://connpass.com/api/v1/event/?"+encodedParams) as res:
+            respons = json.loads(res.read().decode('utf-8'))
+            tweet = respons['events'][0]['started_at'][6:7]+"月"+\
+			        respons['events'][0]['started_at'][8:9]+"日 "+\
+				    respons['events'][0]['title']+"\n"+\
+                    respons['events'][0]['event_url']+"\n#"+respons['events'][0]['hash_tag']
+        post_tweet(tweet)
+    except Exception as e:
+        print('---- print works ---')
+        print(e.message)
+        print('----------print end-')
+
+
+
+    #except Exception as e:
+    #    print('---- print works ---')
+        #print(e.message)
+    #    print('----------print end-')
+
+
+
+
 
 
 def get_event(comm_name):
-	#glnaganoのイベント情報を取得,ついでにnsegさんも
-	url = urllib2.urlopen("https://"+comm_name+".doorkeeper.jp/events/upcoming")
+	url = urllib.request.urlopen("https://"+comm_name+".doorkeeper.jp/events/upcoming")
 
 	soup = BeautifulSoup(url)
 
-	#イベント投稿用urlの一覧を取得
+	#イベントのurlを取得
 	all_url = get_url(soup,comm_name)
 	if all_url is None:
 		return all_url
@@ -64,7 +97,7 @@ def get_tweetlink(all_url):
 	#開催前urlの一覧からtwitter投稿用イベントの一覧を取得する
 	tweet_shares = []
 	for eventurl in all_url:
-		url = urllib2.urlopen(eventurl)
+		url = urllib.request.urlopen(eventurl)
 		soup = BeautifulSoup(url)
 		link = soup.find("a",{"href":"http://twitter.com/share"})
 		title = link.get("data-text")
